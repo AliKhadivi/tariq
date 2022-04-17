17242f261796c3d72696189d38956cfeef8f408e
Ali Khadivi <khadiviali39@gmail.com>
2022-04-17 21:32:11 +0430

    cronjob

---
+++ crontab.sh
@@ -0,0 +1,26 @@
+
+if ! command -v tariq &> /dev/null
+then
+    echo "tariq not installed!"
+    exit 1
+fi
+
+
+#write out current crontab
+sudo crontab -l > /tmp/tariq_cron
+#echo new cron into cron file
+# echo "0 */1 * * * echo hello" >> /tmp/tariq_cron
+crontab="*/15 * * * * tariq reload-ddns"
+
+    if grep -Fxq "$crontab" /etc/tariq/allowed_ddns
+    then
+        echo "Crontab already exist."
+    else
+        sudo echo "$crontab" >> /tmp/tariq_cron
+    fi
+
+
+#install new cron file
+sudo crontab /tmp/tariq_cron
+sudo rm /tmp/tariq_cron
+
